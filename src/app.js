a283733268341c64e7fc294f2a0cf05f2037e294
lufz <lucasfrazzetta@gmail.com>
2023-08-10 00:39:56 -0300

    carpeta public

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import routerProd from './routes/products.routes.js';
+import { __dirname } from './path.js';
 const app = express();
 
 const PORT = 8080;
 
+//Middlewares
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/static', express.static(`${__dirname}/public`));
 app.use('/api/products', routerProd); // defino que mi app va a usar lo que venga en routerProd para la ruta que defina
 
+// Server
 app.listen(PORT, () => {
 	console.log(`Servidor desde puerto: ${PORT}`);
 	console.log(`http://localhost:${PORT}`);
