a1ad4535b20d4610fb907fdf9e50b175007581eb
lufz <lucasfrazzetta@gmail.com>
2023-08-16 18:54:45 -0300

    handlebars

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
 import routerProd from './routes/products.routes.js';
 import routerCart from './routes/cart.routes.js';
 import { __dirname } from './path.js';
+import path from 'path';
 const app = express();
 
 const PORT = 8080;
@@ -10,9 +12,25 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.engine('handlebars', engine()); //defino que mi motor de plantillas va a ser handlebars
+app.set('view engine', 'handlebars');
+app.set('views', path.resolve(__dirname, './views'));
 
 // Routes
-app.use('/static', express.static(`${__dirname}/public`));
+app.use('/static', express.static(path.join(__dirname, '/public')));
+
+app.get('/static', (req, res) => {
+	// indicar que plantilla voy a utilizar
+	const user = {
+		nombre: 'Lucía',
+		cargo: 'Tutor',
+	};
+	res.render('home', {
+		usuario: user,
+		isTutor: user.cargo == 'Tutor', // Tengo que consultarlo antes, en el js porque handlebars no puede manejar operadores logicos
+	});
+});
+
 app.use('/api/products', routerProd); // defino que mi app va a usar lo que venga en routerProd para la ruta que defina
 app.use('/api/carts', routerCart);
 
