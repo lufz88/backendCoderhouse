03bb9f2500a4ac2a905880e1a61ed8209b7151bc
lufz <lucasfrazzetta@gmail.com>
2023-09-05 17:38:20 -0300

    arreglo del carts.routes

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { CartManager } from '../controllers/CartManager.js';
 import cartModel from '../models/carts.models.js';
+import productModel from '../models/products.models.js';
 
 const routerCart = Router();
 const cartManager = new CartManager('./src/models/carts.json', './src/models/products.json');
@@ -51,31 +52,36 @@ routerCart.get('/:cid', async (req, res) => {
 });
 
 routerCart.post('/', async (req, res) => {
-	const { id_prod, quantity } = req.body;
-
 	try {
-		const respuesta = await cartModel.create({
-			id_prod,
-			quantity,
-		});
+		const respuesta = await cartModel.create();
 		res.status(200).send({ resultado: 'OK', message: respuesta });
 	} catch (error) {
 		res.status(400).send({ error: `Error al crear producto: ${error}` });
 	}
 });
 
-routerCart.put('/:cid', async (req, res) => {
-	const { cid } = req.params;
-	const { id_prod, quantity } = req.body;
+routerCart.put('/:cid/product/:pid', async (req, res) => {
+	const { cid, pid } = req.params;
 
 	try {
-		const cart = await cartModel.findByIdAndUpdate(cid, {
-			id_prod,
-			quantity,
-		});
-		cart
-			? res.status(200).send({ resultado: 'OK', message: cart })
-			: res.status(404).send({ resultado: 'Not Found', message: cart });
+		const cart = await cartModel.findById(cid);
+		const product = await productModel.findById(pid);
+
+		if (!product) {
+			res.status(404).send({ resultado: 'Product Not Found', message: product });
+			return false;
+		}
+
+		if (cart) {
+			const productExists = cart.products.find(prod => prod.id === pid);
+			productExists
+				? productExists.quantity++
+				: cart.products.push({ id_prod: product._id, quantity: 1 });
+			await cart.save();
+			res.status(200).send({ resultado: 'OK', message: cart });
+		} else {
+			res.status(404).send({ resultado: 'Cart Not Found', message: cart });
+		}
 	} catch (error) {
 		res.status(400).send({ error: `Error al crear producto: ${error}` });
 	}
